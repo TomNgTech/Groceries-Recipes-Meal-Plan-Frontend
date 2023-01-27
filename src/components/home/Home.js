f59bcb89d9c23cf2386432dfdd473d6f2a842b9a
josh671 <49129783+josh671@users.noreply.github.com>
2023-01-26 17:56:22 -0800

    adding test environment, test not worksing

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,7 +13,7 @@ function HomePage() {
         border: "1px solid black",
         marginTop: "20px",
       }}>
-        <p> Welcome to Meal Plan Assistant. To start add your recipes into the recipes list, there is a lot of detail that can be included into it. After that you can review your meal plan and decide which recipes you want to include on each month. Click on the month to view your current plan for that month and customize it. After you finish adding recipes, export the grocery list from that month and get ready for cooking. </p>
+        <p data-testid="test">Welcome to Meal Plan Assistant. To start add your recipes into the recipes list, there is a lot of detail that can be included into it. After that you can review your meal plan and decide which recipes you want to include on each month. Click on the month to view your current plan for that month and customize it. After you finish adding recipes, export the grocery list from that month and get ready for cooking.</p>
       </body>
     </>
   );
