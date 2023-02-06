8a49dce8feea7bd89f3799b56605ee8f242ea6e8
diegofalconc <diegofalconcostilla94@gmail.com>
2023-02-05 20:42:42 -0800

    Added measurement unit box, Unit and quantity options, delete button

--- src/components/recipes/addrecipe/IngredientOptions.js
+++ src/components/recipes/addrecipe/IngredientOptions.js
@@ -6,12 +6,16 @@ import TableContainer from '@mui/material/TableContainer'
 import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
+import DeleteIcon from '@mui/icons-material/Delete'
 import './IngredientOptions.css'
-import { Button, Checkbox, FormControlLabel, TextField } from '@mui/material'
+import { Button, Checkbox, FormControl, FormControlLabel, InputLabel, MenuItem, Select, TextField } from '@mui/material'
 import { fetchIngredients } from '../../Api'
 
 function IngredientOptions ({ setRecipeIngredient, recipeIngredients }) {
-  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredientData, setNewIngredientData] = useState({
+    ingredientName: '',
+    ingredientUnit: ''
+  })
   const [ingredientsList, setIngredientList] = useState([])
 
   useEffect(() => {
@@ -24,7 +28,8 @@ function IngredientOptions ({ setRecipeIngredient, recipeIngredients }) {
 
   const ingredientOptionsColumns = [
     { id: 'name', label: 'Ingredient', minWidth: 80 },
-    { id: 'add', label: 'Add to Recipe', minWidth: 30 }
+    { id: 'add', label: 'Add', minWidth: 30 },
+    { id: 'delete', label: 'Delete', minWidth: 30 }
   ]
 
   const addIngredientToRecipe = (ingredient, event) => {
@@ -41,13 +46,13 @@ function IngredientOptions ({ setRecipeIngredient, recipeIngredients }) {
   }
 
   const addNewIngredientOption = () => {
-    if (newIngredient !== '' && newIngredient.length !== 50) {
+    if (ingredientData.ingredientName !== '' && ingredientData.ingredientName.length !== 50) {
       setIngredientList((ingredientsList) => [
         ...ingredientsList,
         {
           id: ingredientsList.length + 1,
-          name: newIngredient,
-          measurementType: 'ounces'
+          name: ingredientData.ingredientName,
+          measurementType: ingredientData.ingredientUnit
         }
       ])
     } else {
@@ -55,78 +60,110 @@ function IngredientOptions ({ setRecipeIngredient, recipeIngredients }) {
     }
   }
   return (
-<div className="ingredient_options_container">
-          <Paper className="ingredients_options">
-            <TableContainer className="ingredients_options_table">
-              <Table stickyHeader aria-label="sticky table">
-                <TableHead>
-                  <TableRow>
-                    {ingredientOptionsColumns.map((column) => (
-                      <TableCell
-                        key={column.id}
-                        align={column.align}
-                        style={{ minWidth: column.minWidth }}
-                      >
-                        {column.label}
-                      </TableCell>
-                    ))}
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {ingredientsList.map((ingredient) => {
-                    return (
-                      <TableRow
-                        hover
-                        role="checkbox"
-                        tabIndex={-1}
-                        key={ingredient.id}
-                      >
-                        <TableCell
-                          key={ingredientOptionsColumns.id}
-                          align={ingredientOptionsColumns.align}
-                        >
-                          {ingredient.name}
-                        </TableCell>
-                        <TableCell
-                          key={ingredientOptionsColumns.id}
-                          align={ingredientOptionsColumns.align}
-                        >
-                          <FormControlLabel
-                            control={
-                              <Checkbox
-                                onChange={(e) =>
-                                  addIngredientToRecipe(ingredient, e)
-                                }
-                              />
+    <div className="ingredient_options_container">
+      <Paper className="ingredients_options">
+        <TableContainer className="ingredients_options_table">
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {ingredientOptionsColumns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    align={column.align}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {ingredientsList.map((ingredient) => {
+                return (
+                  <TableRow
+                    hover
+                    role="checkbox"
+                    tabIndex={-1}
+                    key={ingredient.id}
+                  >
+                    <TableCell
+                      key={ingredientOptionsColumns.id}
+                      align={ingredientOptionsColumns.align}
+                    >
+                      {ingredient.name}
+                    </TableCell>
+                    <TableCell
+                      key={ingredientOptionsColumns.id}
+                      align={ingredientOptionsColumns.align}
+                    >
+                      <FormControlLabel
+                        control={
+                          <Checkbox
+                            onChange={(e) =>
+                              addIngredientToRecipe(ingredient, e)
                             }
                           />
-                        </TableCell>
-                      </TableRow>
-                    )
-                  })}
-                </TableBody>
-              </Table>
-            </TableContainer>
-          </Paper>
-          <div className="flex-container">
-            <span className="new_ingredient_span"></span>
-            <TextField
-              id="demo-helper-text-aligned"
-              label="New Ingredient Option"
-              className="new_ingredient_textfield"
-              onChange={(e) => setNewIngredient(e.target.value)}
-            />
-            <span className="new_ingredient_span"></span>
-          </div>
-          <div className="flex-container">
-            <span className="new_ingredient_span"></span>
-            <Button size="medium" onClick={addNewIngredientOption}>
-              {' '}
-              Add to options{' '}
-            </Button>
-            <span className="new_ingredient_span"></span>
-          </div>
-        </div>
+                        }
+                      />
+                    </TableCell>
+                    <TableCell
+                      key={ingredientOptionsColumns.id}
+                      align={ingredientOptionsColumns.align}
+                    >
+                      <DeleteIcon />
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Paper>
+      <div className="flex-container">
+        <span className="new_ingredient_span"></span>
+        <TextField
+          id="demo-helper-text-aligned"
+          label="New Ingredient Option"
+          className="new_ingredient_textfield"
+          onChange={(e) => setNewIngredientData({ ...ingredientData, ingredientName: e.target.value })}
+        />
+        <span className="new_ingredient_span"></span>
+      </div>
+      <div className="flex-container">
+        <span className="new_ingredient_span"></span>
+        <FormControl className="measurementUnitForm">
+          <InputLabel id="demo-simple-select-label">Measurement Unit</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={ingredientData.ingredientUnit}
+            label="Measurement Unit"
+            onChange={(e) => setNewIngredientData({ ...ingredientData, ingredientUnit: e.target.value })}
+          >
+            <MenuItem value={'l'}>liter</MenuItem>
+            <MenuItem value={'ml'}>mililiter</MenuItem>
+            <MenuItem value={'g'}>grams</MenuItem>
+            <MenuItem value={'tbsp'}>tablespoon</MenuItem>
+            <MenuItem value={'tsp'}>teaspoon</MenuItem>
+            <MenuItem value={'oz'}>ounce</MenuItem>
+            <MenuItem value={'c'}>cup</MenuItem>
+            <MenuItem value={'qt'}>quart</MenuItem>
+            <MenuItem value={'gal'}>gallon</MenuItem>
+            <MenuItem value={'lb'}>pound</MenuItem>
+
+          </Select>
+        </FormControl>
+        <span className="new_ingredient_span"></span>
+      </div>
+      <div className="flex-container">
+        <span className="new_ingredient_span"></span>
+        <Button size="medium" onClick={addNewIngredientOption}>
+          {' '}
+          Add to options{' '}
+        </Button>
+        <span className="new_ingredient_span"></span>
+      </div>
+    </div>
   )
 }
 
--- src/components/recipes/addrecipe/RecipeIngredientsTable.js
+++ src/components/recipes/addrecipe/RecipeIngredientsTable.js
@@ -7,58 +7,73 @@ import TableHead from '@mui/material/TableHead'
 import TableRow from '@mui/material/TableRow'
 import Paper from '@mui/material/Paper'
 import './RecipeIngredientsTable.css'
+import { TextField } from '@mui/material'
 
 function RecipeIngredientsTable ({ recipeIngredients }) {
   const recipeIngredientsColumns = [
     { id: 'name', label: 'Recipe Ingredient', minWidth: 80 },
-    { id: 'quantity', label: 'Quantity', minWidth: 30 }
+    { id: 'quantity', label: 'Quantity', minWidth: 30 },
+    { id: 'unit', label: 'Unit', minWidth: 30 }
   ]
 
   return (
-        <Paper className="recipe_ingredients">
-          <TableContainer className="recipe_ingredients_table">
-            <Table stickyHeader aria-label="sticky table">
-              <TableHead>
-                <TableRow>
-                  {recipeIngredientsColumns.map((column) => (
-                    <TableCell
-                      key={column.id}
-                      align={column.align}
-                      style={{ minWidth: column.minWidth }}
-                    >
-                      {column.label}
-                    </TableCell>
-                  ))}
+    <Paper className="recipe_ingredients">
+      <TableContainer className="recipe_ingredients_table">
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {recipeIngredientsColumns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {recipeIngredients.map((ingredient) => {
+              return (
+                <TableRow
+                  hover
+                  role="checkbox"
+                  tabIndex={-1}
+                  key={ingredient.id}
+                >
+                  <TableCell
+                    key={recipeIngredientsColumns.id}
+                    align={recipeIngredientsColumns.align}
+                  >
+                    {ingredient.name}
+                  </TableCell>
+                  <TableCell
+                    key={recipeIngredientsColumns.id}
+                    align={recipeIngredientsColumns.align}
+                    style={{ maxWidth: 60 }}
+                  >
+                    <TextField
+                      hiddenLabel
+                      id="filled-hidden-label-small"
+                      defaultValue="0"
+                      variant="standard"
+                      size="small"
+                    />
+                  </TableCell>
+                  <TableCell
+                    key={recipeIngredientsColumns.id}
+                    align={recipeIngredientsColumns.align}
+                  >
+                    {ingredient.measurementType}
+                  </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {recipeIngredients.map((ingredient) => {
-                  return (
-                    <TableRow
-                      hover
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={ingredient.id}
-                    >
-                      <TableCell
-                        key={recipeIngredientsColumns.id}
-                        align={recipeIngredientsColumns.align}
-                      >
-                        {ingredient.name}
-                      </TableCell>
-                      <TableCell
-                        key={recipeIngredientsColumns.id}
-                        align={recipeIngredientsColumns.align}
-                      >
-                        1
-                      </TableCell>
-                    </TableRow>
-                  )
-                })}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   )
 }
 
