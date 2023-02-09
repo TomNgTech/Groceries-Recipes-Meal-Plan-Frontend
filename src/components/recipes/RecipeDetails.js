cf533eaae31a5b7c084640cd453f701992b4c18d
Joseph Supples <joseph.craig94@gmail.com>
2023-02-09 15:49:01 -0800

    edit recipe via recipeDetails modal

--- src/components/recipes/RecipeDetails.js
+++ src/components/recipes/RecipeDetails.js
@@ -2,7 +2,9 @@ import * as React from 'react'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import DisplayRecipeIngredients from './displayRecipeIngredients'
-import { Button } from '@mui/material'
+import { Button, TextField, Grid } from '@mui/material'
+import RecipeIngredientsTable from './addrecipe/RecipeIngredientsTable.js'
+import IngredientOptions from './addrecipe/IngredientOptions'
 import Container from '@mui/material/Container'
 
 export default function RecipeDetails (props) {
@@ -11,27 +13,70 @@ export default function RecipeDetails (props) {
     transform: 'translate(-50%, -50%)',
     top: '50%',
     left: '50%',
-    width: 800,
     bgcolor: 'background.paper',
     border: '2px solid #000',
+    minWidth: 800,
     boxShadow: 24,
     p: 4
   }
 
-  return (
+  const [edit, setEdit] = React.useState(false)
+  const [recipeName, setRecipeName] = React.useState(props.recipe.dishName)
+  const editRecipe = () => {
+    setEdit(true)
+  }
+
+  const updateRecipe = (recipe) => {
+    setEdit(false)
+    props.handleUpdate(recipe)
+    props.handleClose()
+    // call api to update recipe
+  }
+  if (!edit) {
+    return (
     <Box sx={style}>
       <Typography variant="h3" className="center">
-        {props.dishName}
+        {props.recipe.dishName}
       </Typography>
       <Typography variant="h4" className="center">
         Ingredients
       </Typography>
       <Container>
-        <DisplayRecipeIngredients ingredients={props.ingredients} />
+        <DisplayRecipeIngredients ingredients={props.recipe.ingredients} />
       </Container>
       <div className="center">
+        <Button onClick={editRecipe}>Edit Recipe</Button>
         <Button onClick={props.handleClose}>Close Modal</Button>
       </div>
     </Box>
-  )
+    )
+  } else {
+    return (
+      <Box sx={style}>
+      <Grid container spacing={2} direction="column">
+        <Grid item xs={6} className="center">
+          <TextField
+            label="Recipe Name"
+            defaultValue={recipeName}
+            className="center"
+            onChange={(e) => setRecipeName(e.target.value)}>
+          </TextField>
+        </Grid>
+        <Grid item xs={12} container direction="row" spacing={2} className="center" alignItems="flex-start">
+          <Grid item>
+          <RecipeIngredientsTable recipeIngredients={props.recipe.ingredients} />
+          </Grid>
+          <Grid item>
+          <IngredientOptions recipeIngredients={props.recipe.ingredients} />
+          </Grid>
+
+        </Grid>
+        <Grid item xs={4} className="center">
+          <Button onClick={updateRecipe}>Update Recipe</Button>
+          <Button onClick={props.handleClose}>Close Modal</Button>
+        </Grid>
+      </Grid>
+      </Box>
+    )
+  }
 }
--- src/components/recipes/displayRecipeIngredients.js
+++ src/components/recipes/displayRecipeIngredients.js
@@ -6,19 +6,18 @@ import ListItemText from '@mui/material/ListItemText'
 export default function DisplayRecipeIngredients (props) {
   const items = []
   console.log('length: ' + props.ingredients.length)
-
   for (let i = 0; i < props.ingredients.length; i++) {
     const description = props.ingredients[i].name + ' ' + props.ingredients[i].quantity + ' ' + props.ingredients[i].measurementType
     const item =
-            <ListItem>
-                <ListItemText primary={description} />
-            </ListItem>
+        <ListItem key={props.ingredients[i].name}>
+            <ListItemText primary={description} />
+        </ListItem>
     items.push(item)
   };
 
   return (
-        <List>
-            {items}
-        </List>
+    <List>
+        {items}
+    </List>
   )
 }
