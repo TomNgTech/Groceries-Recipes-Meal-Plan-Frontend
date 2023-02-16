7746c2ced9ee8794fb5a1defb7d19f5620161b9d
Joseph Supples <joseph.craig94@gmail.com>
2023-02-16 11:43:52 -0800

    update and delete api calls

--- src/components/Api.js
+++ src/components/Api.js
@@ -58,7 +58,46 @@ export async function addRecipe (recipe) {
       },
       body: JSON.stringify(recipe)
     })
+    console.log(JSON.stringify(recipe))
     const data = await res.json()
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function updateRecipe (recipe) {
+  const recipeNoId = {
+    dishName: recipe.dishName,
+    ingredients: recipe.ingredients,
+    servingSize: recipe.servingSize
+  }
+  try {
+    const res = await fetch('http://52.37.204.183/recipes/' + recipe.id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(recipeNoId)
+    })
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function deleteRecipe (recipeId) {
+  console.log('recipeid: ' + recipeId)
+  try {
+    const res = await fetch('http://52.37.204.183/recipes/' + recipeId, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await res.json()
+    console.log(data)
     return data
   } catch (err) {
     console.log(err)
