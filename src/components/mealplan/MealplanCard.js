9b4f756d329bafeaeeed0fb72f3b504b608ece86
Joseph Supples <joseph.craig94@gmail.com>
2023-02-23 13:20:25 -0800

    added view meal plan button

--- src/components/mealplan/MealplanCard.js
+++ src/components/mealplan/MealplanCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
-import { Card, CardContent, CardHeader, Container } from '@mui/material'
+import { Button, Card, CardContent, CardHeader, Container, CardActions } from '@mui/material'
 function MealPlanCard (props) {
   return (
         <Card>
@@ -12,6 +12,11 @@ function MealPlanCard (props) {
                 <Typography variant="h6">Recipe 3</Typography>
             </Container>
             </CardContent>
+            <CardActions>
+              <Container className='buttonBox' sx={{ width: 'auto' }}>
+              <Button variant="contained" color="primary" className='button'>View Meal Plan</Button>
+              </Container>
+            </CardActions>
         </Card>
   )
 }
