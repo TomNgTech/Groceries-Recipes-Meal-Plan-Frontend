74d1fa356138ff513cb875d53b031d78eaa1062c
Joseph Supples <joseph.craig94@gmail.com>
2023-03-07 15:37:33 -0800

    removed ingredients portion on tile, got all mealplans and filtered by month

--- src/components/mealplan/Mealplan.js
+++ src/components/mealplan/Mealplan.js
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Mealplan.css'
 import MealPlanCard from './MealplanCard'
 import { Grid } from '@mui/material'
-import { getMealPlanByMonth } from '../Api'
+import { getAllMealPlans } from '../Api'
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 function createGridItems () {
   const gridItems = []
+  const [allplans, setAllplans] = useState([])
+  const [recipeNum, setRecipeNum] = useState([])
+
+  useEffect(() => {
+    async function fetchData () {
+      const fetchedData = getAllMealPlans().then(data => {
+        return Object.values(data)
+      })
+      return fetchedData
+    }
+    fetchData().then(plans => {
+      const recipes = []
+      for (let i = 0; i < months.length; i++) {
+        const monthPlans = plans.filter(element => {
+          return element.month === i
+        })
+        console.log('mealplans for month: ' + i)
+        console.log(monthPlans)
+        recipes[i] = monthPlans.length
+      }
+      return [plans, recipes]
+    }).then(array => {
+      setAllplans(array[0])
+      setRecipeNum(array[1])
+    })
+  }, [])
+
   for (let i = 0; i < months.length; i++) {
-    const mealplans = getMealPlanByMonth((i + 1).toString())
-    console.log('mealplans: ' + mealplans.toString())
-    const gridItem = <Grid item xs={3} key={months[i]}> <MealPlanCard month={months[i]} mealplans={mealplans} /> </Grid>
+    const mealplans = allplans.filter(element => {
+      return element.month === i
+    })
+    const gridItem = <Grid item xs={3} key={months[i]}> <MealPlanCard month={months[i]} mealplans={mealplans} recipeNum={recipeNum[i]}/> </Grid>
     gridItems.push(gridItem)
   }
   return gridItems
--- src/components/mealplan/MealplanCard.js
+++ src/components/mealplan/MealplanCard.js
@@ -1,22 +1,8 @@
 import React from 'react'
 import Typography from '@mui/material/Typography'
 import { Button, Grid, Card, CardContent, CardHeader, Container, CardActions } from '@mui/material'
-import { getRecipeById } from '../Api'
+
 function MealPlanCard (props) {
-  let recipeNum = 0
-  let ingredientNum = 0
-  const weekPlans = props.mealplans.weekInfo
-
-  for (let i = 0; i < props.mealplans.weekInfo.length; i++) {
-    const dishes = weekPlans.where(weekPlans => weekPlans.weekNum === i)
-    console.log(dishes)
-    for (let j = 0; j < dishes.length; j++) {
-      recipeNum++
-      const recipe = getRecipeById(dishes[j].dishId)
-      ingredientNum += recipe.ingredients.length
-    }
-  }
-
   return (
         <Card>
             <CardHeader title={props.month}/>
@@ -24,15 +10,9 @@ function MealPlanCard (props) {
             <Grid container className='recipeBox' spacing={0} >
                 <Grid item xs={6} direction={'column'}>
                   <Container>
-                    <Typography variant="h3">{recipeNum}</Typography>
+                    <Typography variant="h3">{props.recipeNum}</Typography>
                   </Container>
                   <Typography variant="h5">Recipes</Typography>
-                </Grid>
-                <Grid item xs={6} direction={'column'}>
-                  <Container>
-                    <Typography variant="h3">{ingredientNum}</Typography>
-                  </Container>
-                  <Typography variant="h5">Ingredients</Typography>
                 </Grid>
             </Grid>
             </CardContent>
